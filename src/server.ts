85fc97ed11f1af774132ce7092766144c8eb7646
thalles-victor <thalles.victor2012gmail.com>
2024-02-24 17:10:09 -0300

    :sparkles: feat: create server.

---
+++ src/server.ts
@@ -0,0 +1,17 @@
+import "./utils/env.provider";
+
+import express from "express";
+import { envCheckProvider } from "./utils/env.provider";
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (request, response) => {
+  return response.status(200).json({ message: "The server is working" });
+});
+
+const PORT = envCheckProvider.PORT ?? 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running at http://localhost:${PORT}`);
+});
