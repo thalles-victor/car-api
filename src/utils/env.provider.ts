901c0f8eaf5c8c4f90f4e23bdd44e60086100549
thalles-victor <thalles.victor2012gmail.com>
2024-02-24 18:17:37 -0300

    :bug: fix: database port is imported as string in env file. To fix this I added a type conversion step and a regex to only validate strings with numbers.

--- src/utils/env.provider.ts
+++ src/utils/env.provider.ts
@@ -2,6 +2,20 @@ import { z } from "zod";
 
 const envSchema = z.object({
   PORT: z.string().optional(),
+  DATABASE_HOST: z.string(),
+  DATABASE_DB: z.string(),
+  DATABASE_PORT: z
+    .string()
+    .regex(/^[0-9]+$/, { message: "accept only numbers" }),
+  DATABASE_USER: z.string(),
+  DATABASE_PASSWORD: z.string(),
 });
 
-export const envCheckProvider = envSchema.parse(process.env);
+const envSchemaParse = envSchema.parse(process.env);
+
+const transform = {
+  ...envSchemaParse,
+  DATABASE_PORT: parseInt(envSchemaParse.DATABASE_PORT),
+};
+
+export const envCheckProvider = transform;
